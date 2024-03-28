548e180c07f99fb67420eb21b8dee90a94b5ee3c
VladPaliychuk <ssmxtxl@gmail.com>
2024-03-28 15:57:02 +0200

    USerCard.API connected Docker and tested

--- Services/UserCard.API/Program.cs
+++ Services/UserCard.API/Program.cs
@@ -1,23 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using UserCard.API.Data;
+using UserCard.API.Repositories;
+using UserCard.API.Repositories.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddDbContext<CardsContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("UsercardDb")));
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(
+    c => {
+        c.SwaggerDoc("v1", new OpenApiInfo { Title = "UserCard.API", Version = "v1" });
+    }
+);
+
+builder.Services.AddScoped<ICardsRepository, CardsRepository>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(
+        c => {
+            c.SwaggerEndpoint("/swagger/v1/swagger.json", "UserCard.API v1");
+        }
+    );
 }
+
+app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    //endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+    //{
+    //    Predicate = _ => true,
+    //    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+    //});
+});
 
 app.Run();
